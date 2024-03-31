bb0dd2360a7f9d330b35d209e128238fe498f98f
KimYoungWoo <rladuddn3924@gmail.com>
2024-03-31 19:21:16 +0900

    Code and Test of Error Handling from OrderEntry

--- sundae-starter/src/pages/entry/Options.jsx
+++ sundae-starter/src/pages/entry/Options.jsx
@@ -2,20 +2,25 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 import ScoopOption from "./ScoopOption.jsx";
 import { Row } from "react-bootstrap";
+import ToppingOption from "./ToppingOption.jsx";
+import AlertBanner from "../common/AlertBanner.jsx";
 const Options = ({ optionType }) => {
   const [items, setItems] = useState([]);
+  const [error, setError] = useState(false);
   // optionType is 'scoops' or 'toppings'
   useEffect(() => {
     axios
       .get(`http://localhost:3030/${optionType}`)
       .then((response) => setItems(response.data))
-      .catch((error) => {
-        // TODO: handle error response
-      });
+      .catch((error) => setError(true));
   }, [optionType]);
 
+  if (error) {
+    return <AlertBanner />;
+  }
+
   // TODO: response `null` with ToppingOption when available
-  const ItemComponent = optionType === "scoops" ? ScoopOption : null;
+  const ItemComponent = optionType === "scoops" ? ScoopOption : ToppingOption;
 
   const optionItems = items.map((item) => (
     <ItemComponent
